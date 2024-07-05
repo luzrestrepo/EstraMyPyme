38c317190b9ef1dd192102f966f34df39a9e3e9d
Mibarrase <mateoiba9455@hotmail.com>
2024-07-04 20:59:26 -0500

    footer-enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {path: '' , },
+    {path: 'footer' , component: FooterComponent},
+
+];
