3cc5ca5c012ef22892a223c904f1d64c99359186
luz Aidee <luz-aiderc@hotmail.com>
2024-07-11 18:04:09 -0500

    add validations form and styles with talwind

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { passwordConfirmValidator } from '../shared/password-confirm-directive';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule, LoginComponent],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      persona: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      passwordConfirm: new FormControl('', [Validators.required]),
+      terms: new FormControl(false),
+    },
+    { validators: passwordConfirmValidator }
+  );
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+  get email() {
+    return this.registerForm.get('email');
+  }
+  register() {
+    console.log(this.registerForm.value);
+  }
+}
