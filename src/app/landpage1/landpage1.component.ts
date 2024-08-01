3571f15766964d8927b72e3c21b496a4f7ef09c0
EstebanHen <stebanhenao@gmail.com>
2024-07-31 21:13:54 -0500

    Actualizaciones al header

--- src/app/landpage1/landpage1.component.ts
+++ src/app/landpage1/landpage1.component.ts
@@ -1,15 +1,21 @@
 import { Component  } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { InfoComponent } from '../info/info.component';
 
 
 @Component({
   selector: 'app-landpage1',
   standalone: true,
-  imports: [Landpage1Component,RouterOutlet,RouterModule],
+  imports: [Landpage1Component,RouterOutlet,RouterModule, InfoComponent],
   templateUrl: './landpage1.component.html',
   styleUrl: './landpage1.component.css',
   
 })
 
 export class Landpage1Component {   
+  isDropdownVisible: boolean = false;
+
+  toggleDropdown(): void {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
 }
