733cba75cc82421ac61351162e5ebd8607a1eab1
Sebaspj7 <sebastian.perezj7@gmail.com>
2024-08-09 21:53:39 -0500

    Modifique las graficas, elimine el footer y cambie el header

--- src/app/landpage1/landpage1.component.ts
+++ src/app/landpage1/landpage1.component.ts
@@ -1,12 +1,14 @@
 import { Component  } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { InfoComponent } from '../info/info.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-landpage1',
   standalone: true,
-  imports: [Landpage1Component,RouterOutlet,RouterModule, InfoComponent],
+  imports: [Landpage1Component,RouterOutlet,RouterModule, InfoComponent, CommonModule],
   templateUrl: './landpage1.component.html',
   styleUrl: './landpage1.component.css',
   
@@ -17,5 +19,24 @@ export class Landpage1Component {
 
   toggleDropdown(): void {
     this.isDropdownVisible = !this.isDropdownVisible;
+  }
+  showButtons: boolean = true;
+
+  private hideButtonsRoutes: string[] = [
+    '/user/book',
+    '/user/dashboard1',
+    '/user/settings',
+    '/user/charts'
+  ];
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const currentUrl = this.router.url;
+      this.showButtons = !this.hideButtonsRoutes.includes(currentUrl);
+    });
   }
 }
