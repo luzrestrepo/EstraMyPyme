dfb71b275d7eb2b269a465f7e7d2addc2527027d
luz Aidee <luz-aiderc@hotmail.com>
2024-08-09 21:10:44 -0500

    update styles contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
